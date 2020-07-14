2fb2a347590235a02ed4cef21a41748a331b2283
Raymond Pulver IV <raymond.pulver_iv@uconn.edu>
2020-07-14 18:04:48 +0000

    update

--- bin/app.js
+++ bin/app.js
@@ -1,21 +1,23 @@
-'use strict';
+"use strict";
 
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-const dotenv = require('dotenv');
-const path = require('path');
-const createServer = require('../lib/server');
+const dotenv = require("dotenv");
+const path = require("path");
+const createServer = require("../lib/server");
 
 (async () => {
-	dotenv.config(path.join(__dirname, '..'));
-	const { USERNAME, PASSWORD, PORT, HOST } = process.env;
-	const username = USERNAME;
-	const serve = createServer({
-		username,
-		password: PASSWORD
-	});
-	const port = PORT || '8080';
-	const host = HOST || '0.0.0.0';
-	await serve(port, host)
-	console.log(chalk.cyan.bold('BOUND: ') + chalk.bold('[' + host + ':' + port + ']'));
+  dotenv.config(path.join(__dirname, ".."));
+  const { USERNAME, PASSWORD, PORT, HOST } = process.env;
+  const username = USERNAME;
+  const serve = createServer({
+    username,
+    password: PASSWORD,
+  });
+  const port = PORT || "8080";
+  const host = HOST || "0.0.0.0";
+  await serve(port, host);
+  console.log(
+    chalk.cyan.bold("BOUND: ") + chalk.bold("[" + host + ":" + port + "]")
+  );
 })().catch((err) => console.error(err));
--- test/test.js
+++ test/test.js
@@ -1,14 +1,17 @@
-'use strict';
+"use strict";
 
-const dotenv = require('dotenv');
-const path = require('path');
-const { getPlaylist, getEPG } = require('../lib/get');
-const { formatPlaylist } = require('../lib/format');
-dotenv.config(path.join(__dirname, '..'));
+const dotenv = require("dotenv");
+const path = require("path");
+const { getPlaylist, getEPG } = require("../lib/get");
+const { formatPlaylist } = require("../lib/format");
+dotenv.config(path.join(__dirname, ".."));
 
-describe('streamsforus', () => {
-	it('get', async () => {
-    const playlist = await getPlaylist(process.env.USERNAME, process.env.PASSWORD);
-		console.log(formatPlaylist(playlist));
-	});
+describe("streamsforus", () => {
+  it("get", async () => {
+    const playlist = await getPlaylist(
+      process.env.USERNAME,
+      process.env.PASSWORD
+    );
+    console.log(formatPlaylist(playlist));
+  });
 });
